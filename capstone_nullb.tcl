7102bdeda7ee4ff3b45d1bf39360658b35ea5376
Mingkai Li <mingkai.li@hotmail.com>
2024-01-10 07:11:44 +0000

    add null-blk driver tcl script

---
+++ capstone_nullb.tcl
@@ -0,0 +1,31 @@
+set timeout -1
+
+spawn sh start.sh
+
+interact {
+    -o "login: " {
+        send "root\r"
+        return
+    }
+}
+
+interact {
+    -o "# " {
+        send "insmod /capstone.ko\r"
+        send "modprobe configfs\r"
+        return
+    }
+}
+
+interact {
+    -o "# " {
+        send "/null_blk.user\r"
+        send "insmod /nullb/capstone_split/null_blk.ko\r"
+        send "ls -l /dev | grep nullb\r"
+        send "echo \"hello world\" | dd of=/dev/nullb0 bs=1024 count=10\r"
+        send "dd if=/dev/nullb0 bs=1024 count=10 | hexdump -C\r"
+        send "rmmod null_blk\r"
+        send "ls -l /dev | grep nullb\r"
+        send "poweroff -f\r"
+    }
+}
