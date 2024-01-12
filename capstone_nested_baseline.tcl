e6451ecd91c45d65a8b75191620e004785cfce64
Mingkai Li <mingkai.li@hotmail.com>
2024-01-12 06:24:18 +0000

    add nested enclave baseline script

---
+++ capstone_nested_baseline.tcl
@@ -0,0 +1,22 @@
+set timeout -1
+
+spawn sh start.sh
+
+interact {
+    -o "login: " {
+        send "root\r"
+        return
+    }
+}
+
+interact {
+    -o "# " {
+        send "cd /nested/baseline\r"
+        send "./miniweb &\r"
+        send "sleep 1\r"
+        send "busybox wget -O - http://localhost:8888/index.html\r"
+        send "busybox wget -O - http://localhost:8888/magic.html\r"
+        send "busybox wget --post-data \"name=Alex&email=alex@email.com\" -O - http://localhost:8888/cgi/register\r"
+        send "poweroff -f\r"
+    }
+}
