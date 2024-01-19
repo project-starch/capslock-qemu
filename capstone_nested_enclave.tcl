cc8e87cba9fd82821e12bcfe3c3a7c9be051e4a1
Mingkai Li <mingkai.li@hotmail.com>
2024-01-19 13:58:56 +0000

    update nested enclave tcl script to test double entry

--- capstone_nested_enclave.tcl
+++ capstone_nested_enclave.tcl
@@ -24,6 +24,7 @@ interact {
         send "busybox wget -O - http://localhost:8888/null.html\r"
         send "busybox wget -O - http://localhost:8888/register.html\r"
         send "busybox wget --post-data \"name=Alex&email=alex@email.com\" -O - http://localhost:8888/cgi/cgi_register_success.dom\r"
+        send "busybox wget --post-data \"name=Bob&email=bob@email.com\" -O - http://localhost:8888/cgi/cgi_register_success.dom\r"
         send "busybox wget --post-data \"name=Alex&email=alex@email.com\" -O - http://localhost:8888/cgi/cgi_register_fail.dom\r"
         send "poweroff -f\r"
     }
